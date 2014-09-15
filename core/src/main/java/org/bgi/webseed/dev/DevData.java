b9d8a5a13aca3fbb18423f15f57bfaf068c130f6
benoitguillon <benoit.guillon@gmail.com>
2014-09-15 11:52:59 +0200

    trying things

--- core/src/main/java/org/bgi/webseed/dev/DevData.java
+++ core/src/main/java/org/bgi/webseed/dev/DevData.java
@@ -3,9 +3,22 @@ package org.bgi.webseed.dev;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bgi.webseed.api.users.UserDto;
+import org.bgi.webseed.firms.Firm;
+import org.bgi.webseed.users.api.UserDto;
 
 public class DevData {
+	
+	public static List<Firm> allFirms = new ArrayList<Firm>();
+	
+	private static void initializeFirms(){
+		allFirms.add(new Firm(1L, "firm1", "firm1"));
+		allFirms.add(new Firm(2L, "firm2", "firm2"));
+	}
+	
+
+	static {
+		initializeFirms();
+	}
 	
 	public static List<UserDto> getAllUsers(){
 		List<UserDto> result = new ArrayList<>();
