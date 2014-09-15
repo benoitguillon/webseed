9e94e1e62cb02c1b4e198b2dde87f819de530660
benoitguillon <benoit.guillon@gmail.com>
2014-09-15 11:52:42 +0200

    trying things

--- core/src/main/java/org/bgi/webseed/ctrl/RootController.java
+++ core/src/main/java/org/bgi/webseed/ctrl/RootController.java
@@ -1,6 +1,7 @@
 package org.bgi.webseed.ctrl;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +11,16 @@ public class RootController {
 	@RequestMapping
 	public String index(){
 		return "index";
+	}
+	
+	@RequestMapping("partials/{partial}.html")
+	public String partials(@PathVariable String partial){
+		return partial;
+	}
+	
+	@RequestMapping("partials/admin/{partial}.html")
+	public String adminPartials(@PathVariable String partial){
+		return "admin/" + partial;
 	}
 	
 }
